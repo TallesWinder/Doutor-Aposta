d17ba7cbd06db35d57684d814abe5c1c8648dc87
Talles <talleswinder@outlook.com>
2022-07-31 20:56:26 -0300

    Atualizações
    Atualização da página Home e Nossa História

---
+++ PaginaNossaHistoria/script.js
@@ -0,0 +1,12 @@
+let menu_list = document.getElementById("menu-list")
+menu_list.style.maxHeight = "0px"
+
+let show_menu = () => {
+    if (menu_list.style.maxHeight === "0px") {
+        menu_list.style.maxHeight = "120px"
+    }
+
+    else {
+        menu_list.style.maxHeight = "0"
+    }
+}
