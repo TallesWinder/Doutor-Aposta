912c724f62a233bc618ebb27d283a4279d5d988c
Talles <talleswinder@outlook.com>
2022-08-01 09:52:26 -0300

    Atualização
    Descrição de como funciona o JavaScript do menu

--- PaginaComoJogar/script.js
+++ PaginaComoJogar/script.js
@@ -1,12 +1,16 @@
 let menu_list = document.getElementById("menu-list")
-    menu_list.style.maxHeight = "0px"
+    menu_list.style.maxHeight = "0px" //Faz o menu de navegação desaparecer
 
     let show_menu = () => {
+        //Verifica se o menu está fechado
         if (menu_list.style.maxHeight === "0px") {
+
+            //Habilita o menu   
             menu_list.style.maxHeight = "120px"
         }
 
         else {
+            //Faz o menu de navegação desaparecer
             menu_list.style.maxHeight = "0"
         }
     }
